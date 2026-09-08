e90051aaa88e50a2e0a40f8d51509b5d24cb0744
lumalabriandouglas-design <lumalabriandouglas@gmail.com>
2026-09-08 23:18:46 +0000

    Fix web build: hide Flutter 3.47 RepeatMode clash.
    Flutter 3.47 added widgets.RepeatMode, which collided with the app
    repeat enum in player_deck.dart and failed dart2js on Vercel.

--- lib/features/player/widgets/player_deck.dart
+++ lib/features/player/widgets/player_deck.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide RepeatMode;
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
